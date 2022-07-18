8e9f502fdbb4c5d709afd9a5cf5f37a561bec881
rbruggs <rbruggs@gmail.com>
2022-07-17 21:22:29 -0400

    Created core engine systems and components. Implemented ImGui

---
+++ src/Components/SpriteComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <SDL.h>
+
+struct SpriteComponent {
+    std::string assetId;
+    int width;
+    int height;
+    int zIndex;
+    SDL_RendererFlip flip;
+    bool isFixed;
+    SDL_Rect srcRect;
+    
+    SpriteComponent(std::string assetId = "", int width = 0, int height = 0, int zIndex = 0, bool isFixed = false, int srcRectX = 0, int srcRectY = 0) {
+        this->assetId = assetId;
+        this->width = width;
+        this->height = height;
+        this->zIndex = zIndex;
+        this->isFixed = isFixed;
+        this -> flip = SDL_FLIP_NONE;
+        this->srcRect = {srcRectX, srcRectY, width, height};
+    }
+};
---
+++ src/Game/Game.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../ECS/ECS.h"
+#include "../AssetStore/AssetStore.h"
+#include "../EventBus/EventBus.h"
+#include <SDL.h>
+
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
+
+class Game {
+private:
+    bool isRunning;
+    bool isDebug;
+    int millisecsPreviousFrame = 0;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Rect camera;
+
+    std::unique_ptr<Registry> registry;
+    std::unique_ptr<AssetStore> assetStore;
+    std::unique_ptr<EventBus> eventBus;
+
+public:
+    Game();
+    ~Game();
+    void Initialize();
+    void Run();
+    void Setup();
+    void LoadLevel(int level);
+    void ProcessInput();
+    void Update();
+    void Render();
+    void Destroy();
+
+    static int windowWidth;
+    static int windowHeight;
+    static int mapWidth;
+    static int mapHeight;
+};
